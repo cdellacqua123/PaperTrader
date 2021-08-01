9d221baf9465e023d49f391e0356adeea238cb24
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-08-01 13:20:02 -0700

    Created handle input & submit, created basic skeleton for render

--- frontend/components/session/signup_form.jsx
+++ frontend/components/session/signup_form.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
 
 class SignupForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            email: '',
+            password: ''
+        }
+    }
+
+    handleInput(field) {
+        return(e) => {
+            this.setState({ [field]: e.target.value})
+        }
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.signup(this.state)
+    }
+
     render() {
         return(
-            <div></div>
+            <div>
+                <h1>Create a new account:</h1>
+                <form>
+                    <label>Username:
+                        <input
+                        type="text"
+                        value={this.state.username}
+                        onChange={this.handleInput('username')}
+                        />
+                    </label>
+                    <label>Email:
+                        <input
+                            type="text"
+                            value={this.state.email}
+                            onChange={this.handleInput('email')}
+                        />
+                    </label>
+                    <label>Password:
+                        <input
+                            type="text"
+                            value={this.state.password}
+                            onChange={this.handleInput('password')}
+                        />
+                    </label>
+                </form>
+            </div>
         )
     }
 };
