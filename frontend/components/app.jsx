128727680bd70c8e32980b3b238c4d1f632a638d
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-08-01 13:36:46 -0700

    app.jsx now exports signup AuthRoute

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import signup_container from './session/signup_container';
 
 export default () => (
-    <div></div>
-)
+    <div>
+        <AuthRoute path='/signup' component={signup_container}/>
+    </div>
+);
