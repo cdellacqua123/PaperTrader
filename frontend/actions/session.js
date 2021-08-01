5ed00c4e702e3a6d6e111587a6296504bace58ce
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-08-01 10:37:39 -0700

    Added signup, login, & logout to session-actions

--- frontend/actions/session.js
+++ frontend/actions/session.js
@@ -1,2 +1,31 @@
-import {login, signup, logout} from '../util/session'
+import * as APIUtil from '../util/session'
 
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+const receiveErrors = errors => ({
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+});
+
+export const signup = user => dispatch => (
+    APIUtil.signup(user).then(user => (
+        dispatch({ type: 'RECEIVE_CURRENT_USER', user})
+    ), err => (
+        dispatch(receiveErrors(err.responseJSON))
+    ))
+);
+
+export const login = user => dispatch (
+    APIUtil.login(user).then(user => (
+        dispatch({ type: 'RECEIVE_CURRENT_USER', user})
+    ), err => (
+        dispatch(receiveErrors(err.responseJSON))
+    ))
+);
+
+export const logout = () => dispatch => (
+    APIUtil.logout()
+        .then(() => dispatch({ type: 'LOGOUT_CURRENT_USER', }))
+)
