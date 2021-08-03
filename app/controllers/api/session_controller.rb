e99e9038de727b26291082670a0118fb293864b8
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-08-03 10:09:31 -0700

    Created login & logout capabilities in backend

--- app/controllers/api/session_controller.rb
+++ app/controllers/api/session_controller.rb
@@ -1,2 +1,24 @@
 class Api::SessionController < ApplicationController
+    def create
+        @user = User.find_by_credentials(
+            params[:users][:username]
+            params[:users][:password]
+        )
+
+        if @user
+            login(@user)
+            render :show
+        else
+            render json: ["invalid username or password"], status: 401
+        end
+    end
+
+    def destroy
+        @user = current_user
+        if @user
+            logout
+            render '/'
+        else
+            render json: ['Account not signed in'], status: 404
+    end
 end
