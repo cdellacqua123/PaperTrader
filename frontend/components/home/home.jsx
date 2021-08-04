e2162400e40af61c1585e9db9516270de9387d02
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-08-03 21:06:09 -0700

    Adjusted styling of homepage

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -7,14 +7,18 @@ class Home extends React.Component {
     render() {
         return(
             <div className='homepage'>
-            <h1 className='home-h1'>Welcome to PaperTrader</h1>
-                <img className='home-pic'
-                src={exchange}
-                
-                />
-            <Link className='home-create'to="/signup">Create Account</Link>
-            <br></br>
-            <Link className='home-login' to="/login">Login</Link>
+            <h1 className='home-h1'>Paper Trader</h1>
+            <img className='home-pic'src={exchange}/>
+            <div className="home-block">
+                <h1 className='about'>About this website</h1>
+                <p className="description">
+                    This is sample text  that will be changed to include details
+                    about what this site is, how to use it, and who it's for.
+                </p>
+                <button className='home-create'><Link to="/signup">Create Account</Link></button>
+                <br/>
+                <button className='home-login'><Link to="/login">Login</Link></button>
+            </div>
             </div>
         )
     }
