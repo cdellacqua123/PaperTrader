8d421982df1ef2082f56b8e74fa3b3ac9841339c
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-08-04 11:34:28 -0700

    Basic styling for Signup

--- frontend/components/session/signup_form.jsx
+++ frontend/components/session/signup_form.jsx
@@ -27,32 +27,43 @@ class SignupForm extends React.Component {
         return(
             <div>
                 <h1 className='signup-head'>Paper Trader</h1>
-                <h2 className='signup-desc'>Stock Market Simulator</h2>
-                <h1>Create a new account:</h1>
+                <h2 className='signup-head2'>Stock Market Simulator</h2>
+                <h1 className='signup-desc'>Create a new account:</h1>
                 <form>
-                    <label>Username:
+                    <br></br>
+                    <label className='signup-uname'>Username:
+                    <br></br>
                         <input
+                        className='signup-uname-field'
                         type="text"
                         value={this.state.username}
                         onChange={this.handleInput('username')}
                         />
                     </label>
-                    <label>Email:
+                    <br></br>
+                    <label className='signup-email'>Email:
+                    <br></br>
                         <input
+                            className='signup-email-field'
                             type="text"
                             value={this.state.email}
                             onChange={this.handleInput('email')}
                         />
                     </label>
-                    <label>Password:
+                    <br></br>
+                    <label className='signup-pw'>Password:
+                    <br></br>
                         <input
+                            className='signup-pw-field'
                             type="password"
                             value={this.state.password}
                             onChange={this.handleInput('password')}
                         />
                     </label>
-                    
-                    <button onClick={this.handleSubmit} ><Link to="/users/show">Sign Up</Link> </button>
+                    <br></br>
+                    <button className='signup-button' onClick={this.handleSubmit} >
+                        <Link className='signup-link' to="/users/show">Sign Up</Link>
+                    </button>
                     
                 </form>
             </div>
