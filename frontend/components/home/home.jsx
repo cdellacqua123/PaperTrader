09aabcd7c6360d22638d668ab60f797d044ea42e
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-08-03 18:03:52 -0700

    began styling

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import exchange from '../../../app/assets/images/exchange.jpg'
+
 
 class Home extends React.Component {
     render() {
         return(
-            <div>
-            <h1>Welcome to PaperTrader</h1>
-            <Link to="/signup">Create Account</Link>
+            <div className='homepage'>
+            <h1 className='home-h1'>Welcome to PaperTrader</h1>
+                <img className='home-pic'
+                src={exchange}
+                
+                />
+            <Link className='home-create'to="/signup">Create Account</Link>
             <br></br>
-            <Link to="/login">Sign In</Link>
+            <Link className='home-login' to="/login">Login</Link>
             </div>
         )
     }
