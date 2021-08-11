023a405c4eb179e736c44327166c2b803ac82742
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-08-11 10:59:23 -0700

    Updated models to connect positions & trades to accounts

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,2 +1,5 @@
 class Position < ApplicationRecord
+        belongs_to :account,
+        foreign_key: :acct_id,
+        source: :Account
 end
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,2 +1,5 @@
 class Trade < ApplicationRecord
+    belongs_to :account,
+        foreign_key: :acc_id,
+        source: :Account
 end
