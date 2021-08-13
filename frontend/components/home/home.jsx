4ead0d202a17a45f305c7d6d3d81c92fe669ec8c
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-08-12 21:59:56 -0700

    Re-wrote & styled descrption

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -23,8 +23,14 @@ class Home extends React.Component {
             <div className="home-block">
                 <h1 className='about'>About this website</h1>
                 <p className="description">
-                    This is sample text  that will be changed to include details
-                    about what this site is, how to use it, and who it's for.
+                    PaperTrader is a free website that allows users to test
+                    out their stock and/or ETF trading strategies without the risk
+                    of losing their own money. To use it, simply create a a profile
+                    through the "Sign Up" button, create a new trading account with
+                    a nickname and any amount of fake dollars you'd like to start with,
+                    and click "Place Trade". If you'd like to try the website before
+                    creating an account, use the "Guest Login" feature located in 
+                    the "Sign Up" page.
                 </p>
                     <button onClick={this.signUpPage} className='home-create'>Create Profile
                     </button>
