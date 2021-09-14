1b5fe1d925fc8278dee2885df77826409c43092d
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-09-13 21:42:10 -0700

    removed commented out code

--- frontend/components/sidebar/sidebar.jsx
+++ frontend/components/sidebar/sidebar.jsx
@@ -58,57 +58,26 @@ const Sidebar = (accounts) => {
                                 <div className="sidebar">
                                     {/* <FiArrowLeftCircle /> */}
                                     <ul>
-
-
                                     <li className="sidebar-li">
-                                    {/* <h1 > */}
-                                        
-                                    {/* </h1> */}
                                     <Link className='sidebar-links' to={'/users/show'}>Home</Link>
                                     </li>
-                                    
-
-
                                     <li className="sidebar-li">
-                                    {/* <h1> */}
-                                        
-                                    {/* </h1> */}
                                     <Link className='sidebar-links' to={'/users/account/create'}>Create New Account</Link>
                                     </li>
-                                    
-
                                     <li className="sidebar-li">
-                                    {/* <h1> */}
-                                        
-                                    {/* </h1> */}
                                     <Link className='sidebar-links' to={'/users/trades/show'}>Place a Trade</Link>
                                     </li>
-
-
                                     <li className="sidebar-li">
-                                    {/* <h1> */}
-                                        
-                                    {/* </h1> */}
                                     <Link className='sidebar-links' to={'/users/accounts/show'}>View Accounts</Link>
                                     </li>
-                                    
-
                                     <li className="sidebar-li"> 
-                                    {/* <h1> */}
-                                        
-                                    {/* </h1> */}
                                     <Link className='sidebar-links' to={'/users/trades/history'}>Trade History</Link>
                                     </li>
-                                    
-
                                     <li className="sidebar-li">
-                                    {/* <h1> */}
-                                        
-                                    {/* </h1> */}
                                     <Link className='sidebar-links' to={'/users/accounts/edit'}>Edit Accounts</Link>
                                     </li>
                                     </ul>
-                                    </div>
+                                </div>
                             )}
                         </div>
                         {/* <SidebarContent>
