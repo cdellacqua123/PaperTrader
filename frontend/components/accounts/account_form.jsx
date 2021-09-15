9232a6ae44ad64f1982687f82503c212ea2e1e5e
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-09-15 10:40:54 -0700

    create acct now rendering errors

--- frontend/components/accounts/account_form.jsx
+++ frontend/components/accounts/account_form.jsx
@@ -16,16 +16,36 @@ class AccountForm extends React.Component {
         }
     };
 
+    componentDidMount() {
+        this.props.fetchAcctsForUser(this.props.currentUser.id)
+    }
+
     handleSubmit(e){
         e.preventDefault();
-        this.props.submitAcct({
-            account_name: this.state.account_name,
-            balance: this.state.balance,
-            // shorting_allowed: this.state.shorting_allowed,
-            user_id: this.state.user_id
-        });
-        this.props.history.push("/users/show");
-        
+        if ((!this.state) || (!this.state.account_name) || (!this.state.balance)) {
+            this.setState({ 'noValues': true })
+            this.setState({ 'badBal': false })
+            this.setState({ 'badName': false })
+        } else {
+            let check = this.props.accounts.find(account => account.account_name === this.state.account_name)
+            if ((!check) && (parseInt(this.state.balance))) {
+                this.props.submitAcct({
+                    account_name: this.state.account_name,
+                    balance: this.state.balance,
+                    // shorting_allowed: this.state.shorting_allowed,
+                    user_id: this.state.user_id
+                });
+                this.props.history.push("/users/show");
+            } else if (check) {
+                this.setState({'badName': true})
+                this.setState({'badBal': false})
+                this.setState({ 'noValues': false })
+            } else {
+                this.setState({ 'badName': false })
+                this.setState({ 'badBal': true })
+                this.setState({ 'noValues': false })
+            }
+        }
     };
 
     cancel = () => {
@@ -41,17 +61,33 @@ class AccountForm extends React.Component {
         this.props.logout(this.state)
         this.props.history.push('/');
     }
+
+    badInputRender() {
+        if (this.state && this.state.badName) {
+            return (<h1 className='create-acct-name-err'>
+                You Already Have An Account With That Name, Please Select Another
+            </h1>)
+        } else if (this.state && this.state.badBal) {
+            return (<h1 className='create-acct-bal-err'>
+                Please Enter A Numeric Value For Balance
+            </h1>)
+        } else if (this.state && this.state.noValues) {
+            return (<h1 className='create-acct-no-val'>
+                Please Enter A Value In All Fields
+            </h1>)
+        }
+    }
     
     render(){
         return(
             <div>
                 <Header/>
-                {/* <br></br> */}
                 <button className='logout' onClick={this.handleLogout}>Logout
                 </button>
-                <Sidebar/>
+                {/* <Sidebar/> */}
                 <div className='new-acct-container'>
                 <h1 className="new-trade-acct-header">Create a Trading Account</h1>
+                <div>{this.badInputRender()}</div>
                 <form>
                 <h1 className="new-trade-acct-nickname-head">Nickname:</h1><br></br>
                     <input className="new-trade-acct-nickname-input" onChange={this.handleInput('account_name')}/>
