24146285fa8742a7ba16ea20d3e29cf5e5a91fbf
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-09-19 11:25:23 -0700

    adjusted article spacing; changed article timestamp format

--- frontend/components/users/news_article_render.js
+++ frontend/components/users/news_article_render.js
@@ -3,16 +3,21 @@ import React from "react";
 class NewsArticleItem extends React.Component {
     render() {
         const {datetime, headline, image, source, summary, url} = this.props
+        const date = new Date(datetime)
+        const new_datetime = date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear()+' '+date.getHours()+':'+date.getMinutes()+date.getSeconds()
         return(
             <div className='ind-article'>
                 <a href={url} className='article-link'></a>
-                <div>
+                {/* <h1>{url}</h1> */}
+                <div className='article-header'>
                     <h1>{source}</h1>
-                    <h1>{datetime}</h1>
+                    <h1>{new_datetime}</h1>
                 </div>
-                <div>
-                    <h1>{headline}</h1>
-                    <h1>{summary}</h1>
+                <div className='article-info-n-pic'>
+                    <div className='article-headline-n-summary'>
+                        <h1>{headline}</h1>
+                        <h1>{summary}</h1>
+                    </div>
                     <img src={image} className='article-pic'></img>
                 </div>
             </div>
