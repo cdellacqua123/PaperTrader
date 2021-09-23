7afd0d7d19600e350d760d4cdd7d776678248023
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-09-22 21:18:24 -0700

    Added footer err message & style

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -24,14 +24,19 @@ class Footer extends React.Component {
     }
 
     cleanData(api_res) {
-        let keys = Object.keys(api_res.quote['Time Series (Daily)'])
-        let today = api_res.quote['Time Series (Daily)'][keys[0]]
-        let tomorrow = api_res.quote['Time Series (Daily)'][keys[1]]
-        let change = (today['4. close'] - tomorrow['4. close']).toFixed(2)
-        this.setState({['change']: change})
-        this.setState({ ['close']: parseFloat(today['4. close']).toFixed(2)})
-        this.setState({ ['volume']: today['5. volume']})
-        this.setState({['quickQuote']: true})
+        if (api_res.quote['Error Message']) {
+            this.setState({['badTicker']: true})
+        } else {
+            let keys = Object.keys(api_res.quote['Time Series (Daily)'])
+            let today = api_res.quote['Time Series (Daily)'][keys[0]]
+            let tomorrow = api_res.quote['Time Series (Daily)'][keys[1]]
+            let change = (today['4. close'] - tomorrow['4. close']).toFixed(2)
+            this.setState({ ['change']: change })
+            this.setState({ ['close']: parseFloat(today['4. close']).toFixed(2) })
+            this.setState({ ['volume']: today['5. volume'] })
+            this.setState({ ['quickQuote']: true })
+            this.setState({ ['badTicker']: false })
+        }
     }
 
     renderQuote() {
@@ -41,7 +46,8 @@ class Footer extends React.Component {
                 return (
                     <div className="footer-quotes">
                         <h1 className='footer-h1'>Last: ${close}</h1>
-                        <h1 className='footer-h1'>Change: <h1 className='footer-pos-change'>+${change}</h1></h1>
+                        <h1 className='footer-h1'>Change: </h1>
+                        <h1 className='footer-pos-change'>+${change}</h1>
                         <h1 className='footer-h1'>Volume: {volume}</h1>
                     </div>
                 )
@@ -50,7 +56,8 @@ class Footer extends React.Component {
                 return (
                     <div className="footer-quotes">
                         <h1 className='footer-h1'>Last: ${close}</h1>
-                        <h1 className='footer-h1'>Change: <h1 className='footer-neg-change'>-${change}</h1></h1>
+                        <h1 className='footer-h1'>Change: </h1>
+                        <h1 className='footer-neg-change'>-${change}</h1>
                         <h1 className='footer-h1'>Volume: {volume}</h1>
                     </div>
                 )
@@ -63,6 +70,8 @@ class Footer extends React.Component {
                     </div>
                 )
             }
+        } else if (this.state && this.state.badTicker) {
+            return(<h1 className='footer-error'>Please enter a valid ticker symbol</h1>)
         }
     }
 
