8c53252408daf2d4efd12a693e69af11dfabdd2e
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-10-01 17:21:59 -0700

    switched "Sign Up" to "Create Profile"

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -26,7 +26,7 @@ class Home extends React.Component {
                         Paper Trader is a free website that allows users to test
                         out their stock and/or ETF trading strategies without the risk
                         of losing their own money. To use it, simply create a profile
-                        through the "Sign Up" button, create a new trading account with
+                        through the "Create Profile" button, create a new trading account with
                         a nickname and any amount of fake dollars you'd like to start with,
                         and click "Place Trade". If you'd like to try the website before
                         creating an account, use the "Guest Login" feature located in 
